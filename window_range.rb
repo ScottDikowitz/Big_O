25e5cf6744a9378a89472c5c4522db14ee14cd69
App Academy Student <appacademy@Apps-Mac-mini-29.local>
2015-10-08 15:57:41 -0400

    Finish StackQueue implementation

--- window_range.rb
+++ window_range.rb
@@ -12,3 +12,63 @@ def windowed_max_range(array, window_size)
 
   current_max_range
 end
+
+class MyStack
+  def initialize
+    @store = []
+  end
+
+  def pop
+    store.pop
+  end
+
+  def push(val)
+    store.push(val)
+  end
+
+  def peek
+    store.last
+  end
+
+  def size
+    store.length
+  end
+
+  def empty?
+    store.length == 0
+  end
+
+  private
+  attr_reader :store
+end
+
+class StackQueue
+  def initialize
+    @bottom, @top = MyStack.new, MyStack.new
+  end
+
+  def enqueue(val)
+    bottom.push(val)
+  end
+
+  def dequeue
+    if top.empty?
+      until bottom.empty?
+        top.push(bottom.pop)
+      end
+    end
+
+    top.pop
+  end
+
+  def size
+    bottom.size + top.size
+  end
+
+  def empty?
+    size == 0
+  end
+
+  private
+  attr_reader :bottom, :top
+end
