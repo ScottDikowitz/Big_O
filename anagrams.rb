7d077f122bfbf5221b2363c5b8f8121dd01f061b
App Academy Student <appacademy@Apps-Mac-mini-29.local>
2015-10-08 12:00:59 -0400

    finish anagram problems

---
+++ anagrams.rb
@@ -0,0 +1,41 @@
+def first_anagram?(str1, str2)
+  anagrams = str1.split('').permutation.to_a.map(&:join)
+  anagrams.include?(str2)
+end
+
+def second_anagram?(str1, str2)
+  until str1.empty?
+    match = false
+    (0...str2.length).each do |i|
+      if str1[0] == str2[i]
+        str1.slice!(0)
+        str2.slice!(i)
+        match = true
+      end
+    end
+    return false if match == false
+  end
+  str1 == str2
+end
+
+def third_anagram?(str1, str2)
+  str1.sort == str2.sort
+
+end
+
+def fourth_anagram?(str1, str2)
+  freq = Hash.new(0)
+
+  str1.each_char do |char|
+    freq[char] += 1
+  end
+
+  str2.each_char do |char|
+    freq[char] -= 1
+  end
+
+  freq.all? {|k, v| v == 0 }
+end
+
+p first_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("ilevs", "lives")    #=> true
